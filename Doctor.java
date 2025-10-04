06aed467073bfeb27705c35484cebde67c11500a
YacopHJFGK <baochuonght@gmail.com>
2025-10-04 13:14:45 +0700

    Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -8,22 +8,13 @@ public class Doctor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long doctorId;
+
     private String name;
     private String email;
     private String specialization;
 
     @ElementCollection
-    private List<String> availableTimes;
+    private List<String> availableTimes; // Q3 yêu cầu
 
-    // Getters and setters
-    public Long getDoctorId() { return doctorId; }
-    public void setDoctorId(Long doctorId) { this.doctorId = doctorId; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getSpecialization() { return specialization; }
-    public void setSpecialization(String specialization) { this.specialization = specialization; }
-    public List<String> getAvailableTimes() { return availableTimes; }
-    public void setAvailableTimes(List<String> availableTimes) { this.availableTimes = availableTimes; }
+    // getters & setters
 }
