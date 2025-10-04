f70af9e153368e38da0812fd146a2b7794eaf10a
YacopHJFGK <baochuonght@gmail.com>
2025-10-04 13:09:06 +0700

    Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -15,5 +15,15 @@ public class Doctor {
     @ElementCollection
     private List<String> availableTimes;
 
-    // getters & setters
+    // Getters and setters
+    public Long getDoctorId() { return doctorId; }
+    public void setDoctorId(Long doctorId) { this.doctorId = doctorId; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getSpecialization() { return specialization; }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
+    public List<String> getAvailableTimes() { return availableTimes; }
+    public void setAvailableTimes(List<String> availableTimes) { this.availableTimes = availableTimes; }
 }
