a0f3cc08efebe2f909e919f5ce0773a01610d77e
YacopHJFGK <baochuonght@gmail.com>
2025-10-04 13:07:06 +0700

    DoctorController.java

--- DoctorController.java
+++ DoctorController.java
@@ -1,4 +1,4 @@
-package com.clinic.controller;
+package main;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +11,10 @@ public class DoctorController {
     @GetMapping("/{id}/availability")
     public ResponseEntity<?> getDoctorAvailability(@PathVariable Long id,
                                                    @RequestHeader("Authorization") String token) {
-        // TODO: validate token (fake check for now)
         if (!token.startsWith("Bearer ")) {
             return ResponseEntity.status(401).body("Invalid token");
         }
 
-        // fake data for now
         List<String> availableTimes = List.of("2025-10-05 10:00", "2025-10-05 14:00");
         return ResponseEntity.ok(availableTimes);
     }
